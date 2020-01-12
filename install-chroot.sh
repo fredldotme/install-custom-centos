f4447d02589817897f9fd687a102920ea8dd50b4
Justin Van Dort <jtvd78@aol.com>
2020-01-12 10:49:23 -0500

    Fix WAIT_CMD

--- install-chroot.sh
+++ install-chroot.sh
@@ -6,7 +6,7 @@ yum install -y hostname
 yum --releasever=8 install -y yum centos-release
 yum install -y redhat-lsb-core dracut-tools dracut-squash dracut-network dracut-config-rescue dracut-config-generic # is dracut-squash, dracut-network, and dracut-config-generic necessary?
 
-WAIT_CMD
+$WAIT_CMD
 
 # Bootloader
 
@@ -17,16 +17,16 @@ WAIT_CMD
 # https://www.dedoimedo.com/computers/grub2-fedora-command-not-found.html
 # Install grub2-efi-modules
 
-WAIT_CMD
+$WAIT_CMD
 
 yum install -y grub2 grub2-efi efibootmgr
 
-WAIT_CMD
+$WAIT_CMD
 
 # Install kernel 
 yum install -y kernel
 
-WAIT_CMD
+$WAIT_CMD
 
 cat > /etc/default/grub << EOF
 GRUB_TIMEOUT=5
@@ -38,12 +38,12 @@ GRUB_CMDLINE_LINUX="crashkernel=auto rd.auto consoleblank=0"
 GRUB_DISABLE_RECOVERY="true"
 EOF
 
-WAIT_CMD
+$WAIT_CMD
 
 efibootmgr -c -p 1 -d $DRIVE -L "Custom CentOS" -l "\EFI\centos\grubx64.efi"
 grub2-mkconfig -o /boot/efi/EFI/centos/grub.cfg
 
-WAIT_CMD
+$WAIT_CMD
 
 # Networking
 yum install -y NetworkManager
@@ -52,7 +52,7 @@ NETWORKING=yes
 NETWORKING_IPV6=no
 EOF
 
-WAIT_CMD
+$WAIT_CMD
 
 # Journald
 
@@ -65,7 +65,7 @@ EFI_UUID=$(blkid | grep "${DRIVE}1" | sed 's/^.*UUID="\(.*\)" T.*$/\1/')
 BOOT_UUID=$(blkid | grep "${DRIVE}2" | sed 's/^.*UUID="\(.*\)" T.*$/\1/')
 ROOT_UUID=$(blkid | grep "${DRIVE}3" | sed 's/^.*UUID="\(.*\)" T.*$/\1/')
 
-WAIT_CMD
+$WAIT_CMD
 
 cat > /etc/fstab << EOF
 UUID=$EFI_UUID	/boot/efi	vfat	defaults	0	0
@@ -73,12 +73,12 @@ UUID=$BOOT_UUID	/boot		ext4	defaults	0	0
 UUID=$ROOT_UUID	/		ext4	defaults	0	0
 EOF
 
-WAIT_CMD
+$WAIT_CMD
 
 # Root password
 passwd
 
-WAIT_CMD
+$WAIT_CMD
 
 # Time
 rm /etc/localtime
