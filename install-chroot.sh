dbf28e5e63ce5c6919588a69bca9a1704ff7b552
Justin Van Dort <jtvd78@aol.com>
2020-01-12 09:15:16 -0500

    Initial commit

---
+++ install-chroot.sh
@@ -0,0 +1,95 @@
+
+# Reinstall package managers and install base packages
+yum install -y hostname
+yum --releasever=8 install -y yum centos-release
+yum install -y redhat-lsb-core dracut-tools dracut-squash dracut-network dracut-config-rescue dracut-config-generic # is dracut-squash, dracut-network, and dracut-config-generic necessary?
+
+# Install kernel 
+yum install kernel
+
+# Bootloader
+
+# How To Boot For now
+# https://www.linux.com/tutorials/how-rescue-non-booting-grub-2-linux/
+
+# TODO!
+# https://www.dedoimedo.com/computers/grub2-fedora-command-not-found.html
+# Install grub2-efi-modules
+
+yum install -y grub2 grub2-efi efibootmgr
+
+cat > /etc/default/grub << EOF
+GRUB_TIMEOUT=5
+GRUB_DISTRIBUTOR="$(sed 's, release .*$,,g' /etc/system-release)"
+GRUB_DEFAULT=saved
+GRUB_DISABLE_SUBMENU=true
+GRUB_TERMINAL_OUTPUT="console"
+GRUB_CMDLINE_LINUX="crashkernel=auto rd.auto consoleblank=0"
+GRUB_DISABLE_RECOVERY="true"
+EOF
+
+efibootmgr -c -p 1 -d $DRIVE -L "Custom CentOS" -l "\EFI\centos\grubx64.efi"
+grub2-mkconfig -o /boot/efi/EFI/centos/grub.cfg
+
+## Network
+
+# Enable networking
+cat > "/etc/sysconfig/network" << EOF
+NETWORKING=yes
+NETWORKING_IPV6=no
+EOF
+
+# Configure each network device
+init_ifcfg() {
+	if [ "$1" != "lo" ]; then
+		cat > "/etc/sysconfig/network-scripts/ifcfg-${1}" << EOF
+TYPE=Ethernet
+BOOTPROTO=dhcp
+PROXY_METHOD=none
+BROWSER_ONLY=no
+DEFROUTE=yes
+IPV4_FAILURE_FATAL=no
+IPV6INIT=no
+IPV6_AUTOCONF=yes
+IPV6_DEFROUTE=yes
+IPV6_FAILURE_FATAL=no
+NAME=$1
+DEVICE=$1
+ONBOOT=yes
+EOF
+	fi
+}
+
+export -f init_ifcfg
+cut -d: -f1 <(nmcli -t device) | xargs -n1 bash -c 'init_ifcfg "$@"' _
+
+# Journald
+
+
+# Something something autorelabel
+touch /.autorelabel
+
+
+# Fstab (see `blkid`)
+EFI_UUID=$(blkid | grep "${DRIVE}1" | sed 's/^.*UUID="\(.*\)" T.*$/\1/')
+BOOT_UUID=$(blkid | grep "${DRIVE}2" | sed 's/^.*UUID="\(.*\)" T.*$/\1/')
+ROOT_UUID=$(blkid | grep "${DRIVE}3" | sed 's/^.*UUID="\(.*\)" T.*$/\1/')
+
+cat > /etc/fstab << EOF
+UUID=$EFI_UUID	/boot/efi	vfat	defaults	0	0
+UUID=$BOOT_UUID	/boot		ext4	defaults	0	0
+UUID=$ROOT_UUID	/		ext4	defaults	0	0
+EOF
+
+# Root password
+passwd
+
+# Time
+rm /etc/localtime
+ln -s /usr/share/zoneinfo/US/Eastern localtime
+
+# Install custom packages
+yum install vim tmux openssh
+
+# Leave chroot
+exit
---
+++ install.sh
@@ -0,0 +1,40 @@
+export DRIVE=/dev/sda
+
+$ Partition the disk
+parted --script $DRIVE \
+	mklabel gpt \
+	mkpart primary 1MiB 513MiB \
+	mkpart primary 513MiB 1537MiB \
+	mkpart primary 1537MiB 100% \
+	set 1 boot on
+
+# Format the disk
+mkfs.vfat -F32 -n EFI "${DRIVE}1"
+mkfs.ext4 -L BOOT "${DRIVE}2"
+mkfs.ext4 -L ROOT "${DRIVE}3"
+
+# Mount the partitions
+mkdir /target
+mount "${DRIVE}3" /target
+mkdir /target/boot
+mount "${DRIVE}2" /target/boot
+mkdir /target/boot/efi
+mount "${DRIVE}1" /target/boot/efi
+
+# Install yum
+rpm --root /target -i http://mirror.centos.org/centos/8.0.1905/BaseOS/x86_64/os/Packages/centos-release-8.0-0.1905.0.9.el8.x86_64.rpm
+mkdir -p /etc/pki/rpm-gpg && cp /target/etc/pki/rpm-gpg/* /etc/pki/rpm-gpg
+yum -y --installroot=/target --releasever=8 install yum
+
+# Prepare chroot
+cp /etc/resolv.conf /target/etc
+mount --bind /dev/ /target/dev/
+mount -t proc procfs /target/proc/
+mount -t sysfs sysfs /target/sys/
+mount -t efivarfs efivarfs /target/sys/firmware/efi/efivars/
+
+# Enter chroot
+chroot /target
+
+# Unmount
+ mount | grep target | awk '{ print $3 }' | xargs umount -lf
